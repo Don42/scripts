6c1f3aba4f405e24bfcb6375a99e44f1d4632bab
Marco don Kaulea <DonMarco42@gmail.com>
2015-01-21 19:55:44 +0100

    Switch to requests lib, and add some error checking

--- random_rfc.py
+++ random_rfc.py
@@ -13,9 +13,13 @@ Usage:
     random_rfc.py <index>
 """
 
-import urllib.request as req
+import requests
 import random
 import docopt as dopt
+
+
+class PageNotFoundException(Exception):
+    pass
 
 
 def get_random_rfc():
@@ -26,19 +30,29 @@ def get_random_rfc():
                 break
             counter += 1
             return get_rfc(random.randint(1, 9999))
-        except Exception:
+        except requests.ConnectionError:
+            exit(1)
+        except PageNotFoundException:
             continue
 
 
 def get_rfc(number):
     link = "http://www.rfc-editor.org/rfc/rfc{number}.txt".format(number=number)
-    con = req.urlopen(link)
-    return (con.read().decode("utf-8"))
+    r = requests.get(link)
+    r.encoding = 'utf-8'
+    if 200 == r.status_code:
+        return r.text
+    else:
+        raise PageNotFoundException
 
 
 if __name__ == "__main__":
     arguments = dopt.docopt(__doc__)
     if "<index>" in arguments and arguments["<index>"]:
-        print(get_rfc(arguments["<index>"]))
+        try:
+            print(get_rfc(arguments["<index>"]))
+        except PageNotFoundException:
+            print("There is no rfc{}. Try another indey".format(
+                arguments['<index>']))
     else:
         print(get_random_rfc())
