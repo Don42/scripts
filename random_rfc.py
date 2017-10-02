778c0f3a135bcbbfe5b669dfb392b9174bd24dc6
Marco 'don' Kaulea <don@0xbeef.org>
2017-10-02 13:02:54 +0200

    Update email address

--- random_rfc.py
+++ random_rfc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # ----------------------------------------------------------------------------
 # "THE SCOTCH-WARE LICENSE" (Revision 42):
-# <DonMarco42@gmail.com> wrote this file. As long as you retain this notice you
+# <don@0xbeef.org> wrote this file. As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a scotch whisky in return
 # Marco 'don' Kaulea
